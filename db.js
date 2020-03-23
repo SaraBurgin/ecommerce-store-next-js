9186ed68e216996849a2e9dcd78161c7a7f86fdb
Sara <saragonzalezburgin@hotmail.com>
2020-03-23 11:04:05 +0100

    Add Heroku URL and migrate up

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 const postgres = require('postgres');
-const sql = postgres();
+
+//I want to declare a new constant (sql) the result is based on wether the dATABASE_URL is available. If it is available, I want to use it if not I call postgres as empty function.
+const sql = process.env.DATABASE_URL
+  ? postgres(process.env.DATABASE_URL)
+  : postgres();
 
 export async function getAllProducts() {
   const products = await sql`
