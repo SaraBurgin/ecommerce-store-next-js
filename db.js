fc2ff6bae04e3f19ad4162eee5766f18139d3423
Sara <saragonzalezburgin@hotmail.com>
2020-08-13 22:00:36 +0200

    Debugging

--- db.js
+++ db.js
@@ -4,10 +4,6 @@ const postgres = require('postgres');
 //I want to declare a new constant (sql) the result is based on wether the DATABASE_URL is available. If it is available, I want to use it if not I call postgres as empty function. 
 // Process.env.DATABASE_URL is how we access the db to check if it is available.
 
-// postgres://mxuuhhikwvsfln:0f0267309cf578c41306d9803bcf0ae4194e6f9cf8feba4e0dc768cda3a4f1c7@ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/d4cigg0e3rmbdo
-
-
-
 const sql = process.env.DATABASE_URL
   ? postgres(process.env.DATABASE_URL, { ssl: { rejectUnauthorized: false } })
   : postgres();
--- ley.config.js
+++ ley.config.js
@@ -1,14 +1,3 @@
-const config = {
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  username: process.env.PGUSER,
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false,
-  }
-};
-console.log('database configuration: ', config);
 
 module.exports = {
   host: process.env.PGHOST,
