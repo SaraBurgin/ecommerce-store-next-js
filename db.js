00cfc6f2c4ee2a286f2b205d765d6857fba30ccf
Sara <saragonzalezburgin@hotmail.com>
2020-08-13 19:08:04 +0200

    Debug

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
-const postgres = require('postgres').native;
+const postgres = require('postgres');
 
 //I want to declare a new constant (sql) the result is based on wether the DATABASE_URL is available. If it is available, I want to use it if not I call postgres as empty function. 
 // Process.env.DATABASE_URL is how we access the db to check if it is available.
 
-const sql = process.env.DATABASE_URL
-  ? postgres(process.env.DATABASE_URL, { ssl: { rejectUnauthorized: false } })
-  : postgres();
+const sql = process.env.DATABASE_URL ? postgres(process.env.DATABASE_URL, { ssl: true }) : postgres();
+
+// const sql = process.env.DATABASE_URL
+//   ? postgres(process.env.DATABASE_URL, { ssl: { rejectUnauthorized: false } })
+//   : postgres();
 
 export async function getAllProducts() {
   const products = await sql`
