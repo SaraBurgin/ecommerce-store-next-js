9c6c0aabe2c20713eea5b3bb0c0c6be515fdf179
Sara <saragonzalezburgin@hotmail.com>
2020-08-26 12:03:55 +0200

    Responsive cart

--- pages/cart.js
+++ pages/cart.js
@@ -257,6 +257,7 @@ const CartInfo = styled.div`
       font-size: 18px;
       font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
         'Lucida Sans', Arial, sans-serif;
+      text-align: center;
       :hover {
         color: #eecc09;
       }
