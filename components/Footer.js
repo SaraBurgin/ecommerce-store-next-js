2ca8e0a17fd52351a311fc1a5a577922103f72ad
Sara <saragonzalezburgin@hotmail.com>
2020-08-27 13:54:27 +0200

    Responsiveness to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,111 +1,77 @@
 import styled from 'styled-components';
 
-const BreakLine1 = styled.div`
-  display: grid;
-  grid-template-columns: 100vw;
-  height: 0.25em;
-  background-color: #eecc09;
-  list-style: none;
-  justify-items: center;
-  margin: 20px;
-`;
-
 const FooterContainer = styled.div `
 display: grid;
 margin-left: 20px;
 margin-right: 20px;
-grid-template-columns: 0.07fr 0.07fr 1.6fr 1fr 1fr;
-grid-template-rows: 80px;
+grid-template-columns: 1fr;
+grid-template-rows: auto;
+border-top: 4px solid #eecc09;
+border-bottom: 4px solid #eecc09;
 
 .instagram-pic {
+  margin-top: -22px;
   width: 20px;
-}
-.facebook-pic {
-  width: 20px;
+  grid-column: 1;
+  grid-row: 1;
+  justify-self: start;
+  align-self: center;
 }
 
-h1 {
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
-      'Lucida Sans', Arial, sans-serif;
-  color: #737373;
-  font-size: 15px;
+.facebook-pic {
+  margin-bottom: -22px;
+  width: 20px;
+  grid-column: 1;
+  grid-row: 1;
+  justify-self: start;
+  align-self: center;
 }
 
 .cheese-pic {
-  width: 120px; 
+  width: 120px;
+  grid-column: 1;
+  grid-row: 1 / 2; 
+  align-self: center; 
+  justify-self: center;
 }
 
 h2 {
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
-      'Lucida Sans', Arial, sans-serif;
-  font-size: 12px;
+  grid-column: 1;
+  grid-row: 1; 
+  font-family: 'American Typewriter', 'serif';
+  font-size: 13px;
+  margin-right: 10px;
   color: #737373;
   justify-self: end;
-  margin-right: 10px;
-}
-
-@media (max-width: 1000px) {
-  grid-template-columns: 0.07fr 1.45fr 1fr 1fr;
-  grid-template-rows: 80px;
-  h1 {
-    display: none;
-  }
-
-  .facebook-pic {
-    justify-self: start;
+  align-self: center;
+  span {
+    font-weight: bold;
   }
 }
 
-@media (max-width: 935px) {
-  grid-template-columns: 0.07fr 1.3fr 1fr 1fr;
-  grid-template-rows: 80px;
-}
 @media (max-width: 500px) {
-  grid-template-columns: 0.07fr 1fr 1fr 1fr;
-  grid-template-rows: 80px;
-  
   h2 {
-    font-size: 10px;
+    font-size: 9px;
   }
-}
+} 
  `
-
-const BreakLine2 = styled.div`
-  display: grid;
-  grid-template-columns: 100vw;
-  height: 0.25em;
-  background-color: #eecc09;
-  justify-items: center;
-  margin-left: 20px;
-  margin: 20px;
-`;
 
 const Footer = () => (
   <>
-  <BreakLine1>
-      <hr></hr>
-  </BreakLine1>
   <FooterContainer>
       <img className="instagram-pic" src="/images/instagram.png" alt="instagramicon" />
-      <img className="facebook-pic" src="/images/facebook.png" alt="facebookicon" />
-      <h1>
-        Follow us on social media to get all cheesy updates and recommendations!
-      </h1>
+      <img className="facebook-pic" src="/images/facebook.png" alt="facebookicon" /> 
       <img className="cheese-pic" src="/images/logo.png" alt="cheesepic" />
       <h2>
-        <span>Tlfn: +34 934 784 619</span>
+        <span>Tlfn:</span> +34 934 784 619
         <br />
-        <span>info@cheeseit.com</span>
+        <a href="info@cheeseit.com">info@cheeseit.com</a>
         <br />
-        <span>Markgraf Strasse, 59</span>
+        Markgraf Strasse, 59
         <br />
         <span>Wien</span>
       </h2>
-    <hr></hr>
     </FooterContainer>
-    <BreakLine2>
-      <hr></hr>
-  </BreakLine2>
   </>
 );
 
--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,40 @@ import Layout from '../components/Layout'
 import styled from 'styled-components'
 
 const Container = styled.div`
-padding: 50px;
+  display: grid; 
+  grid-template-columns: 1fr;
+  grid-template-rows: 0.4fr 1fr 3fr;
+  font-family: 'American Typewriter', 'serif';
+  justify-items: center;
+  margin: 10px;
+  h1 {
+    color: #eecc09;
+  }
+  h2 {
+  font-size: 25px;
+  padding: 10px;
+  margin-bottom: 20px;
+  justify-self: center;
+  }
+  ul {
+    p {
+      font-weight: bold;
+      font-size: 20px;
+    }
+  }
+  @media(max-width: 900px) {
+    margin-top: 30px;
+    h1 {
+      font-size: 22px;
+      align-self: end;
+    }
+
+    h2 {
+      font-size: 20px;
+      justify-self: center;
+      width: 80vw;
+    }
+  }
 `
 
 const Contact = () => (
@@ -13,7 +46,12 @@ const Contact = () => (
         <title>Contact us</title>
       </Head>
       <Container>
-      <p>Please contact us if you need further information.</p>
+        <h1>We can't cheese it without you!</h1>
+        <h2>So please, if you have any questions, recommendations or sweet things you would like to say to us... BRING IT ON!</h2>
+        <ul><p>Contact details</p>
+          <li><span>Telephone:</span> +34 934 784 619</li>
+          <li><span>E-mail:</span><a href="info@cheeseit.com"> info@cheeseit.com</a></li>
+        </ul>
       </Container>
     </Layout>
   </>
